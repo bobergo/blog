20758e0b0f5fc0d22e276705c74aa6ce3d97e350
bober.dong <bober.dong@zoom.us>
2024-12-27 17:04:16 +0800

    2024-12-27

--- my-blog/go_pub.sh
+++ my-blog/go_pub.sh
@@ -2,7 +2,7 @@ set -x
 cd ../../bobergo.github.io
 git pull
 cd ../blog/my-blog
-hugo -d ../../bobergo.github.io --cleanDestinationDir
+hugo -d ../../bobergo.github.io
 cd ../../bobergo.github.io
 git add .
 git ci -m $(date +"%Y-%m-%d")
